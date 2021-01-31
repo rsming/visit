4a4ea31d1e01c4a0a4848bd1fa637ccc65380858
Rustam Mingazov <rustam.mingazov@gmail.com>
2021-01-31 22:15:43 +1100

    cleaning up

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export CONTEXT_ROOT="visit"
-
-npm run build
-
-aws s3 sync "./static/" "s3://$CONTEXT_ROOT/static" --exclude ".*"
-aws s3 website "s3://$CONTEXT_ROOT" --index-document index.html --error-document index.html
